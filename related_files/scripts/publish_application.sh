d734eca7a15b82ff14ad005814ea58331ed695cb
gcoutu <gc8395@gmail.com>
2025-01-10 21:38:55 -0500

    Exercice Angular

--- related_files/scripts/publish_application.sh
+++ related_files/scripts/publish_application.sh
@@ -12,10 +12,12 @@ so=/home/Exercises/angular-e21/dist/angular-e21/browser
 # Destination (de)
 de=/var/www/html/d002/angular-e21
 
+rm -r $de/assets
 rm $de/favicon.ico
 rm $de/index.html
 rm $de/main*.js
 rm $de/polyfills*.js
 rm $de/styles*.css
+cp -R $so/assets $de/assets
 cp $so/*.* $de/
 sed -i 's/<base href="\/">/<base href="\/d002\/angular-e21\/">/g' $de/index.html
